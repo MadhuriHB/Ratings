7555fca2ba42d4114ce7ad28ac7f44fb1626302c
Hackbright Student <info@hackbrightacademy.com>
2016-08-02 17:21:04 -0700

    templates

--- server.py
+++ server.py
@@ -3,9 +3,15 @@
 from jinja2 import StrictUndefined
 
 from flask import Flask
+
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db, db
+
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
+
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -23,7 +29,15 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """ Show list of users """
+    """ Grabs all the users from the db """
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
 
 
 if __name__ == "__main__":
